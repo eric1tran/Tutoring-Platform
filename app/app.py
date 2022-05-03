08618bc1c92b135b83a6fb8650ed298394c5afd0
Eric Tran <eric1tran@gmail.com>
2022-05-02 23:11:37 -0500

    GET and POST html/backend for signup and login routes

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os, sys
 
 app = Flask(__name__)
@@ -10,19 +10,27 @@ if not os.getenv("DATABASE_URL"):
     print(f"DATABASE_URL is not set. Create a .env file and set it if it doesn't exist")
     sys.exit(1)
 
-@app.route('/')
+
+@app.route('/', methods=['GET','POST'])
 def index():
-    # GET - Display login page
+    # GET - Display user home page if logged in, otherwise the login page
 
     # POST - Validate login information and login and forward to user home page
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
 
     return render_template('login.html')
 
-@app.route('/signup')
+
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
     # GET - Returns HTML forms to get user information
 
     # POST - Calls backend servers to validate user information and create an account
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
 
     return render_template('signup.html')
 
