b9244ee73fbe180262a1c383f4260bc5f583c93c
Eric Tran <eric1tran@gmail.com>
2022-07-05 21:57:14 -0500

    Session cookies for logged in users, right justify navbar links, cleanup app routes

--- app/app.py
+++ app/app.py
@@ -1,10 +1,13 @@
 from dotenv import load_dotenv
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 import os, sys
+
 
 from database import MySqlDBConnection, hash_password
 
 app = Flask(__name__)
+
+app.secret_key = "12345"
 
 load_dotenv()
 
@@ -15,7 +18,11 @@ if not os.getenv("DATABASE_URL"):
 
 # Log In endpoint
 @app.route('/', methods=['GET','POST'])
-def index():
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if 'loggedin' in session and session['loggedin']:
+        print('logged in')
+        return render_template('home.html', name=session['name'])
 
     error = None
 
@@ -25,13 +32,13 @@ def index():
 
         if email == "" or password == "":
             error = "Missing input field(s)"
-            return render_template("index.html", error=error)
+            return render_template("login.html", error=error)
 
         # Validate credentials
         with MySqlDBConnection(os.getenv('DB_HOST'), os.getenv('DB_USER'), os.getenv('DB_PASSWORD'), "tutoring_db") as db:
             cursor = db.session.cursor(dictionary=True)
 
-            query = ("SELECT first, last, email, password FROM users where email = %s")
+            query = ("SELECT id, first, last, email, password FROM users where email = %s")
             cursor.execute(query, (email,))
             data = cursor.fetchall()
             if not data:
@@ -39,16 +46,21 @@ def index():
                 error = "Account/password combination do not exist"
             else:
                 db_result = data[0]
+                user_id = db_result['id']
                 password_db = db_result['password']
                 password_input = hash_password(password)
 
                 if password_db != password_input:
                     error = "Invalid Password!"
                 else:
-                    fullname = f'{db_result["first"]} {db_result["last"]}'
-                    return render_template('home.html', name=fullname)
+                    # Session cookies
+                    session['loggedin'] = True
+                    session['id'] = user_id
+                    session['name'] = f'{db_result["first"]} {db_result["last"]}'
+                    return render_template('home.html', name=session['name'])
+                    #return redirect(url_for('home'))
 
-    return render_template('index.html', error=error)
+    return render_template('login.html', error=error)
 
 # Sign Up endpoint
 @app.route('/signup/', methods=['GET', 'POST'])
@@ -61,6 +73,7 @@ def signup():
         last = request.form['lastname']
         email = request.form['email']
         password = request.form['password']
+
         if email == "" or password == "" or first == "" or last == "":
             error = "Missing input field(s)"
             return render_template("signup.html", error=error)
@@ -91,6 +104,24 @@ def signup():
                 return render_template('home.html', name=f'{first} {last}')
 
     return render_template('signup.html', error=error)
+
+@app.route('/home', methods=['GET','POST'])
+def home():
+    if 'loggedin' in session and session['loggedin']:
+        return render_template('home.html', name=session['name'])
+
+    return redirect(url_for('login'))
+
+@app.route('/profile')
+def profile():
+    return "My Profile"
+
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('name', None)
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
